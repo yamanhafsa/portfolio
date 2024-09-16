fce9339e631a8242c8f12998ac2d49fd6ad08bd5
yamanhafsa <hafsa.yaman.54@gmail.com>
2024-09-16 20:18:37 +0300

    Add files via upload

---
+++ portfolio_tur-main/main.py
@@ -0,0 +1,32 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+
+    email = request.form.get('email')
+    text = request.form.get('text')
+    with open('formGeriBildirimleri.txt', 'a', encoding='utf-8') as f:
+        f.write('email')
+        f.write('text')
+
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
